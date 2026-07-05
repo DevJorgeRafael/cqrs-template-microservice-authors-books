9c1c3a2f592fc4bbb01ebdda6c3565ba103d5e84
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2026-07-04 19:25:38 -0500

    feat: implement CQRS query handlers for retrieving individual authors and books

--- src/modules/authors/application/query/get-author/get-author.handler.ts
+++ src/modules/authors/application/query/get-author/get-author.handler.ts
@@ -3,7 +3,7 @@ import { GetAuthorQuery } from "./get-author.query";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Author } from "src/modules/authors/infrastructure/entities/author.entity";
 import { Repository } from "typeorm";
-import { Logger } from "@nestjs/common";
+import { Logger, NotFoundException } from "@nestjs/common";
 
 @QueryHandler(GetAuthorQuery)
 export class GetAuthorHandler implements IQueryHandler<GetAuthorQuery> {
@@ -13,17 +13,30 @@ export class GetAuthorHandler implements IQueryHandler<GetAuthorQuery> {
         private readonly authorRepository: Repository<Author>,
     ) {}
 
-    async execute(query: GetAuthorQuery): Promise<Author | null> {
+    async execute(query: GetAuthorQuery): Promise<any> {
         try {
             const { id } = query;
-            const author = await this.authorRepository.findOneBy({ id });
+            const author = await this.authorRepository.findOne({
+                where: { id },
+                relations: {
+                    authorsBooks: {
+                        book: true,
+                    },
+                },
+            });
             if (!author) {
                 this.logger.debug(`Author with ID ${id} not found.`);
-                return null;
+                throw new NotFoundException(`Author with ID ${id} not found.`);
             }
 
+            const books = author.authorsBooks?.map((ab) => ab.book).filter(Boolean) || [];
+            const { authorsBooks, ...authorData } = author;
+
             this.logger.debug(`Author with ID ${id} found successfully.`);
-            return author;
+            return {
+                ...authorData,
+                books,
+            };
         } catch (error) {
             this.logger.error(`Error getting author with ID ${query.id}:`, error.stack);
             throw error;
--- src/modules/books/application/query/get-book/get-book.handler.ts
+++ src/modules/books/application/query/get-book/get-book.handler.ts
@@ -1,6 +1,6 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { GetBookQuery } from "./get-book.query";
-import { Logger } from "@nestjs/common";
+import { Logger, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Book } from "src/modules/books/infrastructure/entities/book.entity";
 import { Repository } from "typeorm";
@@ -13,17 +13,30 @@ export class GetBookHandler implements IQueryHandler<GetBookQuery> {
         private readonly bookRepository: Repository<Book>,
     ) {}
 
-    async execute(query: GetBookQuery): Promise<Book | null> {
+    async execute(query: GetBookQuery): Promise<any> {
         try {
             const { id } = query;
-            const book = await this.bookRepository.findOneBy({ id });
+            const book = await this.bookRepository.findOne({
+                where: { id },
+                relations: {
+                    authorsBooks: {
+                        author: true,
+                    },
+                },
+            });
             if (!book) {
                 this.logger.debug(`Book with ID ${id} not found.`);
-                return null;
+                throw new NotFoundException(`Book with ID ${id} not found.`);
             }
 
+            const authors = book.authorsBooks?.map((ab) => ab.author).filter(Boolean) || [];
+            const { authorsBooks, ...bookData } = book;
+
             this.logger.debug(`Book with ID ${id} found successfully.`);
-            return book;
+            return {
+                ...bookData,
+                authors,
+            };
         } catch (error) {
             this.logger.error(`Error getting book with ID ${query.id}:`, error.stack);
             throw error;
